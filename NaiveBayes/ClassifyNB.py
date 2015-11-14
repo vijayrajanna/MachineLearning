b8f3f7ecb2aaa569e7ccec7f464a957e0dc14711
vijayrajanna <vijay.drajanna@gmail.com>
2015-11-14 00:26:02 -0600

    Naive bayes and svm classifier

---
+++ NaiveBayes/ClassifyNB.py
@@ -0,0 +1,18 @@
+def classify(features_train, labels_train):
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+
+    ### your code goes here!
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
+    clf = GaussianNB()
+    clf.fit(features_train,labels_train)
+    predictedClasses = clf.predict(features_train)
+    print predictedClasses
+
+    # Now we need to compute the accuracy.
+    print 'The accuracy of the classification is'
+    print(accuracy_score(labels_train,predictedClasses,normalize=True))
+    return clf #returning the classifier
